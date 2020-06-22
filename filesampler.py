acf8b431a5be89651e1fdc38fc2d46e0fffe8168
mjbo <manueljbo@gmail.com>
2020-06-22 14:33:07 +0200

    Updated samplefiles() with new arguments.

--- filesampler.py
+++ filesampler.py
@@ -6,8 +6,10 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
     output_sampled_files = samplefiles(args.file_folder,
+                                       args.output_folder,
                                        args.file_ext,
                                        args.how_many,
+                                       args.set_seed,
                                        args.saveList)
     return output_sampled_files
 
